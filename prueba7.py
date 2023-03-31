d371dac67d638821bd331a54375f5bafcb3f4d67
raulcamaracarreon <125494150+raulcamaracarreon@users.noreply.github.com>
2023-03-30 21:02:11 -0600

    Update prueba7.py

--- prueba7.py
+++ prueba7.py
@@ -5,7 +5,7 @@ def prueba_ANOVA():
     st.title("Prueba de ANOVA")
 
     # Obtener los datos de la muestra del usuario
-    data = st.text_input("Ingresa los datos de la muestra, separados por comas (ej. 1,2,3;4,5,6;7,8,9): ", value="1,2,3;4,5,6;7,8,9")
+    data = st.text_input("Ingresa los datos de la muestra, -los datos de cada grupo separados por comas y los grupos separados por 'punto y coma'- (ej. 1,2,3;4,5,6;7,8,9): ", value="1,2,3;4,5,6;7,8,9")
 
     # Convertir los datos a una lista de listas de números
     data = [[float(x) for x in row.split(",")] for row in data.split(";")]
